4190ac445ad0b849396c9c567f4e816068192e3b
Manjindersingh28 <mani28au@gmail.com>
2025-05-26 02:13:40 +1000

    Started hero section

---
+++ src/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+        <section id='home'>
+            <div className='hero-left'>
+            <h1>Manjinder Singh</h1> <p>Frontend Developer based in Australia</p>
+            </div>
+            </section>
+            
+    </div>
+  )
+}
+
+export default HeroSection
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './CustomNavbar.jsx';
+import HeroSection from './HeroSection.jsx';
 
 
 
@@ -11,6 +12,7 @@ import CustomNavbar from './CustomNavbar.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CustomNavbar/>
+    <HeroSection/>
     <App />
     
   </StrictMode>,
